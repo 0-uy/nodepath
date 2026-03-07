ffe00256983b09ed2e04bc890da5010ca7fb0413
0-uy <comferre.c@gmail.com>
2026-03-06 21:48:56 -0300

    Delete js directory

--- js/dots.js
+++
@@ -1,9 +0,0 @@
-const stage = document.querySelector('.mockup3d-stage');
-const dots = document.querySelectorAll('.dot');
-
-stage.addEventListener('scroll', () => {
-  const scrollIndex = Math.round(stage.scrollLeft / stage.offsetWidth);
-  dots.forEach((dot, index) => {
-    dot.classList.toggle('active', index === scrollIndex);
-  });
-});
